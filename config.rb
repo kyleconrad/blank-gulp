7f3b2da94be40128619add631dfc61139d1a97c8
Kyle Conrad <kyle.matthew.conrad@gmail.com>
2014-01-20 20:49:25 -0500

    Line movement

--- config.rb
+++ config.rb
@@ -2,8 +2,8 @@
 
 # Set this to the root of your project when deployed:
 http_path = "./prod"
-css_dir = "css"
 sass_dir = "sass"
+css_dir = "css"
 images_dir = "img"
 javascripts_dir = "js"
 fonts_dir = "fonts"
